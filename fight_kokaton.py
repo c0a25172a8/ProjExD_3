31692c9b476326248d38b4e339978a6cf9ff2033
nasa <c0a25172a8@edu.teu.ac.jp>
2026-06-23 14:58:13 +0900

    練習3と4　被弾時表情とGameOverの文字

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -162,7 +162,12 @@ def main():
         if bomb is not None:
             if bird.rct.colliderect(bomb.rct):
                 # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
-                bird.change_img(6, screen)
+                bird.change_img(8, screen)
+
+                fonto = pg.font.Font(None, 80)
+                txt = fonto.render("Game Over", True, (255, 0, 0))
+                screen.blit(txt, [WIDTH//2-150, HEIGHT//2])
+
                 pg.display.update()
                 time.sleep(1)
                 return
@@ -171,6 +176,7 @@ def main():
             if beam.rct.colliderect(bomb.rct):
                 beam = None
                 bomb = None
+                bird.change_img(6, screen)
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
